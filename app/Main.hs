a765cb379bc0ee6279ed95ee75494bfe21506a38
Hackle Wayne <hacklew@hotmail.com>
2017-03-12 11:24:23 +1300

    String interpolation

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,11 @@ module Main where
 
 import System.Environment
 import Samsum
+import Text.Printf
 
 main :: IO ()
 main = do 
         arg1:arg2:_ <- getArgs
         let     [min, max]  = map (\x -> read x::Int) [arg1, arg2]
                 result      = findBetween min max
-            in putStr $ show min ++ " -> " ++ (show max) ++ "and result is " ++ (show result)
+            in putStr $ printf "%d -> %d and result is %s" min max (show result)

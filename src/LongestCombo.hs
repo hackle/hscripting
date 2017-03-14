e3e2ee5a1788b9b51338a9f8a031959121cd3167
Hackle Wayne <Hackle.Wayne@trademe.co.nz>
2017-03-14 18:20:16 +1300

    Correct

--- src/LongestCombo.hs
+++ src/LongestCombo.hs
@@ -9,7 +9,7 @@ longestList :: [[a]] -> [a]
 longestList xss = maximumBy (compare `on` length) xss
 
 findComboWithTolerance :: Int -> [Int] -> [Int]
-findComboWithTolerance tolerance _ | tolerance == maxTolerance = []
+findComboWithTolerance tolerance _ | tolerance > maxTolerance = []
 findComboWithTolerance _ [] = []
 findComboWithTolerance _ (x:[]) = [x]
 findComboWithTolerance tolerance (x:x1:xs) =  if inOrder
@@ -18,7 +18,11 @@ findComboWithTolerance tolerance (x:x1:xs) =  if inOrder
                                     where
                                         inOrder = x < x1
                                         thisContinued = x : (findComboWithTolerance 0 (x1:xs))
-                                        moreTolerant = findComboWithTolerance 1 (x:xs)
+                                        moreTolerant = findComboWithTolerance (tolerance + 1) (x:xs)
 
 longestCombo :: [Int] -> [Int]
 longestCombo xs = longestList [ findComboWithTolerance 0 ys | ys <- scanr (:) [] xs ]
+
+-- longestCombo [ 1, 2, 3, 2, 6, 4, 7, 6, 6, 7, 10, 11 ] = [1,2,3,4,6,7,10,11]
+-- longestCombo [1, 2, 3, 5, 4, 5] = [1,2,3,4,5]
+-- longestCombo [1, 2, 3, 5, 5, 5, 4, 5] = [1,2,3,5]
